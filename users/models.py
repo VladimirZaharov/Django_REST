209eb71fe8176fa8f20373a1f7e8c4e6ce39db2e
ноут <vovazakharov2@gmail.com>
2022-02-27 14:11:01 +0300

    done

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid4)
+    username = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
