92efb1af627468cbca30bd2bc41eed1944b28abf
ноут <vovazakharov2@gmail.com>
2022-03-12 15:52:56 +0300

    changed project.serializer

--- project/serializers.py
+++ project/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer, StringRelatedField
 from .models import Project
+from users.models import User
 
+
+class UserProjectModelSerializator(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',)
 
 class ProjectModelSerializer(ModelSerializer):
-    working_users = StringRelatedField(many=True)
+    working_users = UserProjectModelSerializator(many=True)
 
     class Meta:
         model = Project
