0caf04994dbce8397baa59831745ff17262b3e12
ноут <vovazakharov2@gmail.com>
2022-04-17 19:37:31 +0300

    done

--- service/settings.py
+++ service/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_filters',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -140,10 +141,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS':  ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
-    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication',
-                                       'rest_framework.authentication.SessionAuthentication',
-                                       'rest_framework.authentication.TokenAuthentication',
-                                       ]
-
+    # 'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.BasicAuthentication',
+    #                                    'rest_framework.authentication.SessionAuthentication',
+    #                                    'rest_framework.authentication.TokenAuthentication',
+    #                                    ],
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
 }
--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from users.views import UserModelViewSet
 from project.views import ProjectModelViewSet
@@ -9,14 +11,24 @@ from todo.views import ToDoModelViewSet
 
 
 router = DefaultRouter()
-router.register('users', UserModelViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todo', ToDoModelViewSet)
+
+
+schema_view = get_schema_view(openapi.Info(title="Library", default_version='0.1',
+                                           description="Documentation to out project",
+                                           contact=openapi.Contact(email="admin@admin.local"),
+                                           license=openapi.License(name="MIT License"),
+                                           ), public=True,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
+    re_path(r'^api/(?P<version>\d\.\d)/users/$', UserModelViewSet.as_view({'get': 'list'})),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
